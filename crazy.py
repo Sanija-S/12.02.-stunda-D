b2c78a9b1d51d2cce872c8a1588bf34860b14704
Sanija Sokirka <sanijasokirka@gmail.com>
2024-02-12 08:42:46 +0200

    1234567890

---
+++ crazy.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg # pip vajadzigs
+# izkārtojuma definīcija- jānosauc visas loga daļas 
+
+daļas=[
+[sg.Text("Izvēlies failu, kuru nolasīt:")]
+
+
+
+]
+#loga definicija- definejam objektu- logs
+
+logs=sg.Window
